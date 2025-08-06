1fc637752e19f50a0626bc0a859aacfb38511454
solomonarts <solomonarts2023@gmail.com>
2025-08-06 19:00:10 +0300

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "stealthmark1",
+  base: "",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
